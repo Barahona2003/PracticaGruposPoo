82614e6997c811943e7f581eb40bcad13e975aaa
varakaza <121120855+Barahona2003@users.noreply.github.com>
2024-04-07 16:09:47 +0200

    first hello world

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace PracticaGruposPoo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
